782ef2e20355d1776819b14c2627b279f350e43c
oxtornado <dnielussa@gmail.com>
2025-03-31 09:52:45 -0500

    register Moto model in admin with custom display fields

--- motos_api/admin.py
+++ motos_api/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Moto
 
 # Register your models here.
+class MotoAdmin(admin.ModelAdmin):
+    list_display = (
+        'reference',
+        'trademark',
+        'model',
+        'price',
+        'image',
+        'supplier',
+    )
+    
+# Register your models here.
+admin.site.register(Moto, MotoAdmin)
