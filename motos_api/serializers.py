cfde7aac9d9c6609b3f26223ec7103a46727a769
oxtornado <dnielussa@gmail.com>
2025-03-31 10:14:33 -0500

    add MotoSerializer for the Moto model with specified fields

---
+++ motos_api/serializers.py
@@ -0,0 +1,16 @@
+# motos_api/serializers.py
+from rest_framework import serializers
+from .models import Moto
+
+class MotoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Moto
+        fields = (
+            'id',
+            'reference',
+            'trademark',
+            'model',
+            'price',
+            'image',
+            'supplier',
+        )
